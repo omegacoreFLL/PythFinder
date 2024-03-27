144078cb507ca991c0822a701dcc81db548cc07c
Omega Core <omegacoresincai@gmail.com>
2024-03-27 08:08:41 +0200

    alpha

---
+++ BetterClasses/MathEx.py
@@ -0,0 +1,86 @@
+import math
+
+#---Point--- and ---Pose--- classes, used for localisation
+class Point:
+    def __init__(self, x = 0, y = 0):
+        self.x = x
+        self.y = y  
+    
+    def set(self, x, y):
+        self.x = x 
+        self.y = y 
+    
+    def rotateMatrix(self, angle):
+        copy = self.x
+        self.x = self.x * math.cos(angle) - self.y * math.sin(angle)
+        self.y = copy * math.sin(angle) - self.y * math.cos(angle)
+
+        return self
+
+class Pose(Point):
+    def __init__(self, x = 0, y = 0, head = 0):
+        super().__init__(x, y)
+        self.head = head 
+
+    def set(self, x, y, head):
+        super().set(x, y)
+        self.head = head 
+
+
+#enhanced math functions
+def rotateMatrix(x, y, angle):
+    rotated_x = x * math.cos(angle) - y * math.sin(angle)
+    rotated_y = x * math.sin(angle) - y * math.cos(angle)
+
+    return Point(rotated_x, rotated_y)
+
+def normalizeDegrees(deg):
+    while deg >= 360:
+        deg -= 360
+    while deg < 0:
+        deg +=360
+
+    return deg  
+
+def normalizeRadians(rad):
+    while rad >= 2 * math.pi:
+        rad -= 2 * math.pi
+    while rad < 0:
+        rad += 2 * math.pi
+
+    return rad   
+
+def toRadians(deg):
+   return deg * math.pi / 180
+
+def toDegrees(rad):
+    return rad * 180 / math.pi
+
+def hypot(x, y):
+    return math.sqrt(x * x + y * y)
+
+def signum(x):
+    if x < 0:
+        return -1
+    return 1
+
+def msToS(ms):
+    return ms / 1000
+
+def sToMs(sec):
+    return sec * 1000
+
+def clipMotor(value):
+    if value < -100:
+        value = -100
+    elif value > 100:
+        value = 100
+    return value
+
+def findShortestPath(current_angle, target_angle):
+    error = target_angle - current_angle
+    error_abs = abs(error)
+
+    if (error_abs <= 360 - error_abs):
+        return -error
+    return signum(error) * 360 - error
---
+++ constants.py
@@ -0,0 +1,172 @@
+from BetterClasses.MathEx import *
+import pygame
+
+
+default_width = 1500
+default_length = 1000
+half_w = default_width / 2
+half_l = default_length / 2
+
+frame_rate = 100 #fps
+
+default_background_color = "black"
+default_coordinate_system_color = "white"
+default_text_color = "white"
+default_grid_color = (63, 63, 63) #rgb
+default_system_font = 'graffitiyouthregular'
+
+#pixels
+pixels_to_decimeters = 100
+positive_direction_arrow = 25
+half_unit_measure_line = 10
+
+robot_image_source = "Images/bot_from_above.png"
+robot_width = 15 #cm
+robot_length = 20 #cm
+
+turn_sensitivity = 0.04
+robot_wheel_distance = 9.97 / turn_sensitivity  #cm
+
+robot_scaling_factor = 1 #from field dimensions
+
+robot_velocity = 20 #cm / sec | has an error of +- 0.03 cm
+multiplier = 1.97
+
+menu_color = (0, 100, 0)
+fade_percent = 0.9
+time_until_fade = 0.45 #sec
+fade_l = 200
+fade_w = 300
+
+trail_len = 2000 #pixels
+trail_color = "yellow"
+trail_width = 2
+
+
+velocity_multiplier = multiplier * robot_velocity * pixels_to_decimeters / 10
+
+robot_width_in_pixels = robot_scaling_factor * (robot_width * pixels_to_decimeters / 10)
+robot_length_in_pixels =  robot_scaling_factor * (robot_length * pixels_to_decimeters / 10)
+
+button_stop_joystick = True
+using_joystick = False
+manual_control = True
+field_centric = True
+tracking = False
+
+
+#KEY BINDS
+left_wheel_forward_key = pygame.K_q
+right_wheel_forward_key = pygame.K_w
+
+left_wheel_backward_key = pygame.K_a
+right_wheel_backward_key = pygame.K_s
+
+turn_0_key = pygame.K_1
+turn_45_key = pygame.K_2
+turn_90_key = pygame.K_3
+turn_135_key = pygame.K_4
+turn_180_key = pygame.K_5
+turn_225_key = pygame.K_6
+turn_270_key = pygame.K_7
+turn_315_key = pygame.K_8
+
+
+
+
+#XBOX
+xbox_threshold = 0.0001
+xbox_left_x = 0
+xbox_left_y = 1
+xbox_right_x = 2
+
+xbox_disable_button = 2
+xbox_direction_button = 3
+xbox_zero_button = 1
+xbox_menu_button = 5
+xbox_trail_button = 0
+
+xbox_turn_0 = (0, 1)
+xbox_turn_45 = (1, 1)
+xbox_turn_90 = (1, 0)
+xbox_turn_135 = (1, -1)
+xbox_turn_180 = (0, -1)
+xbox_turn_225 = (-1, -1)
+xbox_turn_270 = (-1, 0)
+xbox_turn_315 = (-1, 1)
+
+#PS4
+ps4_threshold = 0.06
+ps4_left_x = 0
+ps4_left_y = 1
+ps4_right_x = 2
+
+ps4_disable_button = 2
+ps4_direction_button = 3
+ps4_zero_button = 1
+ps4_menu_button = 10
+ps4_trail_button = 0
+
+ps4_turn_0 = 11
+ps4_turn_45 = None
+ps4_turn_90 = 14
+ps4_turn_135 = None
+ps4_turn_180 = 12
+ps4_turn_225 = None
+ps4_turn_270 = 13
+ps4_turn_315 = None
+
+#PS5
+ps5_threshold = 0.05 #to be tuned
+ps5_left_x = 0
+ps5_left_y = 1
+ps5_right_x = 2
+
+ps5_disable_button = 2
+ps5_direction_button = 3
+ps5_zero_button = 1
+ps5_menu_button = 10
+ps5_trail_button = 0
+
+ps5_turn_0 = (0, 1)
+ps5_turn_45 = (1, 1)
+ps5_turn_90 = (1, 0)
+ps5_turn_135 = (1, -1)
+ps5_turn_180 = (0, -1)
+ps5_turn_225 = (-1, -1)
+ps5_turn_270 = (-1, 0)
+ps5_turn_315 = (-1, 1)
+
+
+
+
+def toFieldCoords(pose):
+    return Pose(half_l - pose.y, pose.x - half_w, normalizeDegrees(pose.head - 90))
+    
+def toWindowCoords(pose):
+    return Pose(half_w + pose.y, half_l - pose.x, normalizeDegrees(pose.head + 90))
+
+def pixels_to_cm(point):
+    return Point(point.x / pixels_to_decimeters * 10, point.y / pixels_to_decimeters * 10)
+
+    #refers to Field coordinate system
+
+def symmetrical(point, axis):
+    if axis == "x":
+        new_x = 2 * half_w - point.x
+        return Point(new_x, point.y)
+
+    if axis == "y":
+        new_y = 2 * half_l - point.y
+        return Point(point.x, new_y)
+
+    raise Exception("Not a valid ---axis---")
+
+def point2Tuple(point):
+    return (point.x, point.y)
+
+def tuple2Point(tuple):
+    return Point(tuple[0], tuple[1])
+
+def percent2Alpha(value):
+    return value * 2.25
