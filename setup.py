8ba28ee8dbee978a2bff2bc4ca3ccf58f4fdd91a
omegacoreFLL <omegacoresincai@gmail.com>
2024-08-29 15:57:49 +0300

    v0.0.5.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'pythfinder',
-    version = '0.0.5.0',
+    version = '0.0.5.1',
     license = 'MIT',
     author = 'Contraș Adrian',
     author_email = 'omegacoresincai@gmail.com',
